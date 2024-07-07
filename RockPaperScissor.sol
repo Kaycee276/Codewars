823938b9ce8fd9f7c5142681591c61ee1466365b
Izuaba Kenneth Kelechukwu <144479895+Kaycee276@users.noreply.github.com>
2024-07-07 13:46:07 +0100

    I made some changes in the comment

--- RockPaperScissor.sol
+++ RockPaperScissor.sol
@@ -20,12 +20,12 @@ contract RockPaperScissors {
   event GameStarted(address[] players, uint gameNumber);
   event GameComplete(address winner, uint gameNumber);
   
-  // /**
-  //  * Use this endpoint to create a game. 
-  //  * It is a payable endpoint meaning the creator of the game will send ether directly to it.
-  //  * The ether sent to the contract should be used as the bet for the game.
-  //  * @param {address} participant - The address of the participant allowed to join the game.
-  //  */
+  /**
+   * Use this endpoint to create a game. 
+   * It is a payable endpoint meaning the creator of the game will send ether directly to it.
+   * The ether sent to the contract should be used as the bet for the game.
+   * @param participant - The address of the participant allowed to join the game.
+  */
   function createGame(address payable participant) public payable {
     require(msg.value > 0, "You have to deposit some ETH");
 
@@ -42,13 +42,13 @@ contract RockPaperScissors {
     emit GameCreated(msg.sender, gameCounter, msg.value);
   }
   
-  // /**
-  //  * Use this endpoint to join a game.
-  //  * It is a payable endpoint meaning the joining participant will send ether directly to it.
-  //  * The ether sent to the contract should be used as the bet for the game. 
-  //  * Any additional ether that exceeds the original bet of the creator should be refunded.
-  //  * @param {uint} gameNumber - Corresponds to the gameNumber provided by the GameCreated event
-  //  */
+ /**
+  * Use this endpoint to join a game.
+  * It is a payable endpoint meaning the joining participant will send ether directly to it.
+  * The ether sent to the contract should be used as the bet for the game. 
+  * Any additional ether that exceeds the original bet of the creator should be refunded.
+  * @param gameNumber - Corresponds to the gameNumber provided by the GameCreated event
+*/
   function joinGame(uint gameNumber) public payable {
     Game storage game = games[gameNumber];
     require(game.isActive, "Game does not exist or is already completed");
@@ -63,11 +63,11 @@ contract RockPaperScissors {
 
   }
   
-  // /**
-  //  * Use this endpoint to make a move during a game 
-  //  * @param {uint} gameNumber - Corresponds to the gameNumber provided by the GameCreated event
-  //  * @param {uint} moveNumber - The move for this player (1, 2, or 3 for rock, paper, scissors respectively)
-  //  */
+ /**
+  * Use this endpoint to make a move during a game 
+  * @param gameNumber - Corresponds to the gameNumber provided by the GameCreated event
+  * @param moveNumber - The move for this player (1, 2, or 3 for rock, paper, scissors respectively)
+ */
   function makeMove(uint gameNumber, uint moveNumber) public { 
     Game storage game = games[gameNumber];
     require(game.isActive, "Game is invalid");
